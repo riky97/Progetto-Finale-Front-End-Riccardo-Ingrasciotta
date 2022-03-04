38ba33c131ccbf68af1df801deff1ed18287ca59
riky97 <71893803+riky97@users.noreply.github.com>
2022-03-04 19:33:58 +0100

    anime list

--- src/component/allAnimeList/AllAnimeList.jsx
+++ src/component/allAnimeList/AllAnimeList.jsx
@@ -3,9 +3,11 @@ import { Row, Col, Table, List } from "antd";
 import axios from "axios";
 
 import AnimeCard from "../card/AnimeCard";
+import useWindowDimensions from "../shared/UseWindowDimensions";
 
 const AllAnimeList = () => {
   const [generalTop, setGeneralTop] = useState([]);
+  const { height, width } = useWindowDimensions();
   const split = localStorage.getItem("more").split("/");
   const path = split[split.length - 1];
   console.log(path);
@@ -13,14 +15,17 @@ const AllAnimeList = () => {
   useEffect(() => {
     const anime = async () => {
       const res = await getAnime();
-      console.log(res);
+
       setGeneralTop(res.top);
     };
     anime();
-  });
+  }, []);
   const getAnime = async () => {
     const split = localStorage.getItem("more").split("/");
-    const path = split[split.length - 1];
+    let path = split[split.length - 1];
+    if (path === "anime") {
+      path = "tv";
+    }
     const options = {
       method: "GET",
       url: `https://jikan1.p.rapidapi.com/top/anime/1/${path}`,
@@ -34,10 +39,37 @@ const AllAnimeList = () => {
     return data;
   };
 
+  const page = (width) => {
+    let numPagination;
+    let countPageSize = 2;
+
+    if (width < 576) {
+      numPagination = 1 * countPageSize;
+    }
+    if (width >= 576) {
+      numPagination = 2 * countPageSize;
+    }
+    if (width >= 768) {
+      numPagination = 2 * countPageSize;
+    }
+    if (width >= 992) {
+      numPagination = 3 * countPageSize;
+    }
+    if (width >= 1200) {
+      numPagination = 4 * countPageSize;
+    }
+    if (width >= 1600) {
+      numPagination = 6 * countPageSize;
+    }
+    return numPagination;
+  };
+
   return (
     <>
       <div className="section-title">
-        <h3>{path}</h3>
+        <h3>
+          {generalTop.length} {path}
+        </h3>
       </div>
       <List
         loading={false}
@@ -53,6 +85,7 @@ const AllAnimeList = () => {
           defaultCurrent: 1,
           position: "bottom",
           className: "pagination-home",
+          pageSize: page(width),
         }}
         dataSource={generalTop}
         renderItem={(rec) => (
