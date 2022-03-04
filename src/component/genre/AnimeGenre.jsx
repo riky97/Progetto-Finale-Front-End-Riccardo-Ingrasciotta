9f7e2b93aef0b9b02abfb2905a1233c6d4bd2207
riky97 <71893803+riky97@users.noreply.github.com>
2022-03-04 18:57:05 +0100

    genre

--- src/component/genre/AnimeGenre.jsx
+++ src/component/genre/AnimeGenre.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import "./genre.css";
+import { List, message, Avatar } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
 const AnimeGenre = () => {
-  return <h1 style={{ color: "#fff" }}>genre</h1>;
+  const data = [
+    {
+      mal_id: 1,
+      name: "Action Anime",
+    },
+    {
+      mal_id: 2,
+      name: "Adventure Anime",
+    },
+    {
+      mal_id: 3,
+      name: "Cars Anime",
+    },
+    {
+      mal_id: 4,
+      name: "Comedy Anime",
+    },
+    {
+      mal_id: 5,
+      name: "Avant Garde Anime",
+    },
+    {
+      mal_id: 6,
+      name: "Demons Anime",
+    },
+    {
+      mal_id: 7,
+      name: "Mystery Anime",
+    },
+    {
+      mal_id: 8,
+      name: "Drama Anime",
+    },
+    {
+      mal_id: 10,
+      name: "Fantasy Anime",
+    },
+    {
+      mal_id: 11,
+      name: "Game Anime",
+    },
+    {
+      mal_id: 14,
+      name: "Horror Anime",
+    },
+    {
+      mal_id: 15,
+      name: "Kids Anime",
+    },
+  ];
+  const setInLocal = (value) => {
+    localStorage.setItem("genre", value);
+  };
+
+  return (
+    <>
+      <List
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item className="genre-list-anime" key={item.mal_id}>
+            <List.Item.Meta title={item.name} />
+            <div>
+              <a
+                href={`/genre/${item.name.replace(/ /g, "").toLowerCase()}`}
+                style={{ color: " #fff" }}
+                onClick={() => setInLocal(item.mal_id)}
+              >
+                <ArrowRightOutlined style={{ fontSize: "1.8em" }} />
+              </a>
+            </div>
+          </List.Item>
+        )}
+      />
+      <hr />
+    </>
+  );
 };
 
 export default AnimeGenre;
