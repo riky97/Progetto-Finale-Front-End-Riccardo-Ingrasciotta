118f37f9b1d5ac5fefd584860703d537a8b1be2a
riky97 <71893803+riky97@users.noreply.github.com>
2022-03-04 17:41:45 +0100

    genre

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@
 import "./component/home/home.css";
 import "./component/partials/partials.css";
 
+//REACT ROUTER DOM
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 //COMPONENT
 import AnimeSidebar from "./component/partials/AnimeSidebar";
 import AnimeFooter from "./component/partials/AnimeFooter";
 import RouterHome from "./component/home/RouterHome";
+import AnimeGenre from "./component/genre/AnimeGenre";
 
 //SHARED
 import { getPathName } from "./component/shared/getPathName";
@@ -13,7 +17,7 @@ import useWindowDimensions from "./component/shared/UseWindowDimensions";
 
 //ANTD
 import { Layout, Menu, Breadcrumb } from "antd";
-import { useEffect } from "react";
+import { SmileOutlined } from "@ant-design/icons";
 const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
@@ -27,10 +31,10 @@ function App() {
         className="layout"
         style={width <= 768 ? { marginLeft: 0 } : { marginLeft: 200 }}
       >
-        <Header
+        {/* <Header
           className="site-layout-background"
           style={{ padding: 0 }}
-        ></Header>
+        ></Header> */}
         <Content
           style={
             width <= 768
@@ -39,10 +43,24 @@ function App() {
           }
         >
           <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item className="breadcrump">
+              <h2>
+                Welcome Visitor{" "}
+                <SmileOutlined
+                  style={{ fontSize: "1.4rem", color: "yellow" }}
+                />
+              </h2>
+              <p>Today's home</p>
+            </Breadcrumb.Item>
           </Breadcrumb>
+          <hr />
           <div className="site-layout-content">
             <RouterHome />
+            <Router>
+              <Routes>
+                <Route path="/genre" element={<AnimeGenre />} />
+              </Routes>
+            </Router>
           </div>
         </Content>
         <AnimeFooter />
---
+++ src/component/genre/AnimeGenre.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnimeGenre = () => {
+  return <h1 style={{ color: "#fff" }}>genre</h1>;
+};
+
+export default AnimeGenre;
